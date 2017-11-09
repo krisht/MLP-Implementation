08fa9ba1607b10859759db20cae9d69606bd906d
Krishna Thiyagarajan <krisht@cooper.edu>
2017-11-09 16:18:57 -0500

    Forward prop done

--- MLP.py
+++ MLP.py
@@ -1,6 +1,8 @@
 import numpy as np
+from scipy.special import expit as sigmoid
 
 
+# noinspection PyPep8Naming
 class MultiLayerPerceptron:
     def __init__(self, arch):
         self.arch = arch
@@ -12,21 +14,18 @@ class MultiLayerPerceptron:
         self.num_layers = len(arch) - 1
 
         for ii in range(self.num_layers):
-            self.weights['w_%d_%d' % (ii, ii + 1)] = np.random.randint(0, 5, arch[ii:ii+2])
-
-        print(self.weights)
+            self.weights['w_%d_%d' % (ii, ii + 1)] = np.random.randint(0, 5, arch[ii:ii + 2])
 
     def forward_prop(self, X):
-        X = X.T
         if len(X.shape) != 2 or X.shape[1] != self.arch[0]:
-            raise ValueError("Incorrect input given!")
+            raise ValueError("Incorrect input shape " + str(X.shape) + " given!")
         else:
             self.y = None
             for ii in range(self.num_layers):
                 if self.y is None:
-                    self.y = np.matmul(X, self.weights["w_%d_%d" % (ii, ii + 1)])
+                    self.y = sigmoid(np.matmul(X, self.weights["w_%d_%d" % (ii, ii + 1)]))
                 else:
-                    self.y = np.matmul(self.y,  self.weights["w_%d_%d" % (ii, ii + 1)])
+                    self.y = sigmoid(np.matmul(self.y, self.weights["w_%d_%d" % (ii, ii + 1)]))
 
         return self.y
 
@@ -42,5 +41,4 @@ class MultiLayerPerceptron:
 
 if __name__ == "__main__":
     mlp = MultiLayerPerceptron([3, 2, 3, 4, 5])
-    print(mlp.forward_prop(np.random.randint(0, 5, (3, 1))))
-
+    print(mlp.forward_prop(np.random.randint(0, 5, (4, 3))))
