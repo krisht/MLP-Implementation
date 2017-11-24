8d7169fe8ce84d5f96bc8161b3922f1f17efc03f
Krishna Thiyagarajan <krisht@cooper.edu>
2017-11-24 04:02:50 -0500

    It started working with Sable's format

--- MLP.py
+++ MLP.py
@@ -1,129 +1,153 @@
 import numpy as np
 import os
 from scipy.special import expit as sig
+#
+# def dsig(X):
+# 	return sig(X) * (1 - sig(X))
 
-def dsig(X):
-    return sig(X) * (1 - sig(X))
-
+# def sig(X):
+# 	return sig(X)
 
 class MultiLayerPerceptron:
-    def __init__(self,  input_file=None, epoch=None, lambduh=None):
-        self.num_epochs = epoch
-        self.lambduh = lambduh
-        self.weights = {}
-        self.biases = {}
-        arch = [0, 0, 0]
+	def __init__(self,  init_weights_file=None, data_file=None,  epoch=None, lambduh=None):
+		self.data_file = data_file
+		self.init_weights_file = init_weights_file
+		self.num_epochs = epoch
+		self.lambduh = lambduh
+		self.arch = []
+		self.weights = {}
+		self.biases = {}
 
-        # with open(input_file, 'r') as file:
-        #     arch = [int(i) for i in file.readline().split()]
-        #     weights = []
-        #     for line in file:
-        #         line = [float(i) for i in line.split()]
-        #         weights = weights + line
-        #
-        #     W * x + b
-        #
-        #
-        #     self.weights['w_0_1'] = np.reshape(np.asarray(weights[:(arch[0])*(arch[1])]), (arch[0], arch[1]))
-        #     self.biases['b_0_1'] = np.reshape(np.asarray(weights[(arch[0])*(arch[1]): (arch[0])*(arch[1] + 1)]), (1, arch[1]))
-        #     self.weights['w_1_2'] = np.reshape(np.asarray(weights[(arch[1] + 1) * arch[2] : ]), (arch[1] + 1, arch[2]))
-        #     self.biases['b_1_2'] = np.reshape(np.asarray(weights[(arch[1])*(arch[2]): (arch[1]+1)*(arch[2])]), (1, arch[2]))
-        #
-        #
-        #     #print(len(weights[(arch[0] + 1)*(arch[1]):]))
-        #
-        #     print(self.weights['w_0_1'].shape)
-        #     print(self.weights['w_1_2'].shape)
-        #
-        #     print(self.biases['b_0_1'].shape)
-        #     print(self.biases['b_1_2'].shape)
+		self.load_initial_weights()
 
-    #     self.arch = arch
-    #     self.weights = {}
-    #     self.biases = {}
-    #
-    #     self.y = None
-    #
-    #     self.num_layers = len(arch) - 1
-    #
-    #     for ii in range(self.num_layers):
-    #         self.weights['w_%d_%d' % (ii, ii + 1)] = np.zeros(arch[ii:ii + 2])
-    #
-    # def forward_prop(self, x):
-    #     if len(x.shape) != 2 or x.shape[1] != self.arch[0]:
-    #         raise ValueError("Incorrect input shape " + str(x.shape) + " given!")
-    #     else:
-    #         self.y = None
-    #         for ii in range(self.num_layers):
-    #             if self.y is None:
-    #                 self.y = sig(np.matmul(x, self.weights["w_%d_%d" % (ii, ii + 1)]))
-    #             else:
-    #                 self.y = sig(np.matmul(self.y, self.weights["w_%d_%d" % (ii, ii + 1)]))
-    #
-    #     return self.y
-    #
-    # def load_initial_weights(self, file):
-    #
-    #     pass
-    #
-    #
-    # def back_prop(self, trainX, trainY, gamma, lamduh):
-    #     pass
-    #
-    #
-    # def test_network(self):
-    #     pass
-    #
-    # def train_network(self):
-    #     pass
+		self.y = None
+
+		self.num_layers = len(self.arch) - 1
+
+		self.X_train, self.y_train = self.get_data()
+
+	def get_data(self):
+
+		data = []
+		with open(self.data_file, 'r') as file:
+			temp = file.readline().split()
+			num_samples = int(temp[0])
+			num_inputs = int(temp[1])
+			num_outputs = int(temp[2])
+
+			for line in file:
+				line = [float(i) for i in line.split()]
+				data = data + [line]
+			x = np.asarray(data).T[:num_inputs].T
+			y = np.asarray(data).T[num_inputs:].T
+		return x, y
+
+	def forward_prop(self, x):
+		if len(x.shape) != 2 or x.shape[1] != self.arch[0]:
+			raise ValueError("Incorrect input shape " + str(x.shape) + " given!")
+		else:
+			self.y = None
+			for ii in range(self.num_layers):
+				if self.y is None:
+					self.y = sig(np.matmul(x, self.weights["w_%d_%d" % (ii, ii + 1)]))
+					self.y = self.y - self.biases["b_%d_%d" % (ii, ii + 1)]
+				else:
+					self.y = sig(np.matmul(self.y, self.weights["w_%d_%d" % (ii, ii + 1)]))
+					self.y = self.y - self.biases["b_%d_%d" % (ii, ii + 1)]
+
+		print(self.y.shape)
+		return self.y
+
+	def load_initial_weights(self):
+		with open(self.init_weights_file, 'r') as file:
+			arch = [int(i) for i in file.readline().split()]
+
+			weights = []
+			for ii in range(arch[1]):
+				line = file.readline()
+				line = [float(i) for i in line.split()]
+				weights = weights + [line]
+			w_0_1 = np.asarray(weights).T
+
+			weights = []
+			for ii in range(arch[2]):
+				line = file.readline()
+				line = [float(i) for i in line.split()]
+				weights = weights + [line]
+			w_1_2 = np.asarray(weights).T
+
+			self.arch = arch
+			self.biases['b_0_1'] = w_0_1[-1]
+			self.weights['w_0_1'] = w_0_1[0:-1]
+			self.biases['b_1_2'] = w_1_2[-1]
+			self.weights['w_1_2'] = w_1_2[0:-1]
+
+
+	def back_prop(self, trainX, trainY, gamma, lamduh):
+		pass
+
+
+	def test_network(self):
+		pass
+
+	def train_network(self):
+		pass
 
 
 def __train_neural_network__():
-    print("Neural Network Training Program")
+	#print("Neural Network Training Program")
 
-    # Request text file with original weights
-    initial_weights_path = input("Enter initial weight file location: ")
-    while not os.path.isfile(initial_weights_path):
-        initial_weights_path = input("Enter initial weight file location: ")
+	# # Request text file with original weights
+	# initial_weights_path = input("Enter initial weight file location: ")
+	# while not os.path.isfile(initial_weights_path):
+	# 	initial_weights_path = input("Enter initial weight file location: ")
+	#
+	# # Request
+	# training_set_path = input("Enter training set file location: ")
+	# while not os.path.isfile(training_set_path):
+	# 	training_set_path = input("Enter training set file location: ")
+	#
+	# output_path = input("Enter output file location: ")
+	# while not os.path.isfile(output_path):
+	# 	output_path = input("Enter output file location: ")
+	#
+	# num_epochs = int(input("Enter positive integer for number of epochs: "))
+	# while not num_epochs > 0:
+	# 	num_epochs = int(input("Enter positive integer for number of epochs: "))
+	#
+	# lambduh = float(input("Enter learning rate: "))
+	# while not lambduh > 0.0:
+	# 	lambduh = float(input("Enter learning rate: "))
 
-    # Request
-    training_set_path = input("Enter training set file location: ")
-    while not os.path.isfile(training_set_path):
-        training_set_path = input("Enter training set file location: ")
+	initial_weights_path = '/home/krishna/Dropbox/MultiLayerPerceptron/sampleB.trained'
+	num_epochs = 10
+	lambduh = 5
+	data_file_path = '/home/krishna/Dropbox/MultiLayerPerceptron/sampleB.train'
 
-    output_path = input("Enter output file location: ")
-    while not os.path.isfile(output_path):
-        output_path = input("Enter output file location: ")
-
-    num_epochs = int(input("Enter positive integer for number of epochs: "))
-    while not num_epochs > 0:
-        num_epochs = int(input("Enter positive integer for number of epochs: "))
-
-    lambduh = float(input("Enter learning rate: "))
-    while not lambduh > 0.0:
-        lambduh = float(input("Enter learning rate: "))
+	net = MultiLayerPerceptron(initial_weights_path, data_file_path, num_epochs, lambduh)
+	rand_input = np.random.rand(50, 30)*100
+	#print(rand_input)
+	print(net.forward_prop(net.X_train))
 
 
 def __test_neural_network__():
-    print("Neural Network Testing Program")
+	#print("Neural Network Testing Program")
 
-    # Request text file with original weights
-    trained_weights = input("Enter trained neural network file: ")
-    while not os.path.isfile(trained_weights):
-        trained_weights = input("Enter trained neural network file: ")
+	# Request text file with original weights
+	trained_weights = input("Enter trained neural network file: ")
+	while not os.path.isfile(trained_weights):
+		trained_weights = input("Enter trained neural network file: ")
 
-    # Request
-    testing_set_path = input("Enter testing set file location: ")
-    while not os.path.isfile(testing_set_path):
-        testing_set_path = input("Enter testing set file location: ")
+	# Request
+	testing_set_path = input("Enter testing set file location: ")
+	while not os.path.isfile(testing_set_path):
+		testing_set_path = input("Enter testing set file location: ")
 
-    output_path = input("Enter output file location: ")
-    while not os.path.isfile(output_path):
-        output_path = input("Enter output file location: ")
-
-
-# if __name__ == "__main__":
-#     __train_neural_network__()
+	output_path = input("Enter output file location: ")
+	while not os.path.isfile(output_path):
+		output_path = input("Enter output file location: ")
 
 
-MultiLayerPerceptron(input_file='./sample.init')
+if __name__ == "__main__":
+	__train_neural_network__()
+
