ef96fb9cb0194c5a59463fd5104e358721af2627
Krishna Thiyagarajan <krisht@cooper.edu>
2017-12-06 09:41:54 -0500

    Updated program to check for import errors. Need to check if it works.

--- MLP.py
+++ MLP.py
@@ -1,24 +1,48 @@
 import os
 import sys
 
-import numpy as np
-from scipy.special import expit as sig
-from sklearn.datasets import make_multilabel_classification
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
+try:
+	import numpy as np
+except ImportError:
+	sys.stdout.write('Script requires numpy...\n')
+	sys.exit(-1)
+
+try:
+	from scipy.special import expit as sig
+except ImportError:
+	sys.stdout.write('Script requires expit from scipy.special...\n')
+	sys.exit(-1)
+
+try:
+	from sklearn.datasets import make_multilabel_classification
+except ImportError:
+	sys.stdout.write('Script requires make_multilabel_classification from sklearn.datasets...\n')
+	sys.exit(-1)
+
+try:
+	from sklearn.metrics import confusion_matrix
+except ImportError:
+	sys.stdout.write('Script requires make_multilabel_classification from sklearn.datasets...\n')
+	sys.exit(-1)
+
+try:
+	from sklearn.model_selection import train_test_split
+except ImportError:
+	sys.stdout.write('Script requires train_test_split from sklearn.model_selection...\n')
+	sys.exit(-1)
 
 try:
 	assert sys.version_info >= (3,)
 except AssertionError:
-	sys.stdout.write("Python 3 interpreter required\n")
+	sys.stdout.write("Run with python3...\n")
+	sys.stdout.write("usage: python3 MLP.py [train|test]\n")
 	sys.exit(-1)
 
 np.set_printoptions(precision=3)
 
 
-class MultiLayerPerceptron:
-	def __init__(self, weights_file=None, train_file=None, test_file=None, output_file=None,
-				 num_epochs=None, alpha=None):
+class MLP:
+	def __init__(self, weights_file=None, train_file=None, test_file=None, output_file=None, num_epochs=None, alpha=None):
 		self.train_file = train_file
 		self.test_file = test_file
 		self.weights_file = weights_file
@@ -60,8 +84,10 @@ class MultiLayerPerceptron:
 			for line in file:
 				line = [float(i) for i in line.split()]
 				data = data + [line]
+
 			x = np.asarray(data).T[:num_inputs].T
 			y = np.asarray(data).T[num_inputs:].T
+
 			return x, y
 		return None, None
 
@@ -108,26 +134,29 @@ class MultiLayerPerceptron:
 			orig_results[ii, 0], orig_results[ii, 3] = orig_results[ii, 3], orig_results[ii, 0]
 
 		orig_results = np.asarray(orig_results, dtype=np.int32)
+		accuracy, precision, recall, f1 = calculate_metrics(orig_results)
 
-		accuracy_score, precision, recall, f1 = calculate_metrics(orig_results)
+		accuracy = np.expand_dims(accuracy, 0).T
+		precision = np.expand_dims(precision, 0).T
+		recall = np.expand_dims(recall, 0).T
+		f1 = np.expand_dims(f1, 0).T
 
-		results = np.concatenate((orig_results, np.expand_dims(accuracy_score, 0).T, np.expand_dims(precision, 0).T,
-								  np.expand_dims(recall, 0).T, np.expand_dims(f1, 0).T), 1)
+		results = np.concatenate((orig_results, accuracy, precision, recall, f1), 1)
 
 		temp = np.average(results[:, 4:], axis=0)
 		temp[3] = 2 * temp[1] * temp[2] / (temp[1] + temp[2])
 
-		if self.output_file is not None:
-			with open(self.output_file, 'wb') as f:
-				for ii in range(results.shape[0]):
-					tmp_str = "%d %d %d %d %0.3f %0.3f %0.3f %0.3f\n" % tuple(results[ii, :])
-					f.write(tmp_str.encode('utf-8'))
-				tmp_str = "%0.3f %0.3f %0.3f %0.3f\n" % calculate_metrics(np.sum(orig_results, axis=0, keepdims=True))
+		with open(self.output_file, 'wb') as f:
+			for ii in range(results.shape[0]):
+				tmp_str = "%d %d %d %d %0.3f %0.3f %0.3f %0.3f\n" % tuple(results[ii, :])
 				f.write(tmp_str.encode('utf-8'))
-				tmp_str = "%0.3f %0.3f %0.3f %0.3f\n" % tuple(temp)
-				f.write(tmp_str.encode('utf-8'))
-		else:
-			return np.average(accuracy_score)
+			tmp_str = "%0.3f %0.3f %0.3f %0.3f\n" % calculate_metrics(np.sum(orig_results, axis=0, keepdims=True))
+			f.write(tmp_str.encode('utf-8'))
+			tmp_str = "%0.3f %0.3f %0.3f %0.3f\n" % tuple(temp)
+			f.write(tmp_str.encode('utf-8'))
+
+		# Return value not used in most cases
+		return np.average(accuracy)
 
 	def train_network(self):
 
@@ -141,13 +170,13 @@ class MultiLayerPerceptron:
 				self.weights['w2'] = self.weights['w2'] + self.alpha * np.matmul(self.a2.T, delta3)
 				self.weights['w1'] = self.weights['w1'] + self.alpha * np.matmul(self.a1.T, delta2)
 
-		if self.output_file is not None:
-			with open(self.output_file, 'wb') as f:
-				tmp_str = '%d %d %d\n' % (self.arch[0], self.arch[1], self.arch[2])
-				f.write(tmp_str.encode('utf-8'))
+		with open(self.output_file, 'wb') as f:
+			tmp_str = '%d %d %d\n' % (self.arch[0], self.arch[1], self.arch[2])
+			f.write(tmp_str.encode('utf-8'))
 
-			np.savetxt(open(self.output_file, 'ab'), self.weights['w1'].T, '%0.3f', delimiter=' ')
-			np.savetxt(open(self.output_file, 'ab'), self.weights['w2'].T, '%0.3f', delimiter=' ')
+		np.savetxt(open(self.output_file, 'ab'), self.weights['w1'].T, '%0.3f', delimiter=' ')
+		np.savetxt(open(self.output_file, 'ab'), self.weights['w2'].T, '%0.3f', delimiter=' ')
+
 
 def __train_neural_network__():
 	print("Neural Network Training Program")
@@ -177,8 +206,7 @@ def __train_neural_network__():
 	while not alpha > 0.0:
 		alpha = float(input("Enter learning rate: "))
 
-	net = MultiLayerPerceptron(weights_file=weights_file, train_file=train_path, output_file=output_path,
-							   num_epochs=num_epochs, alpha=alpha)
+	net = MLP(weights_file=weights_file, train_file=train_path, output_file=output_path, num_epochs=num_epochs, alpha=alpha)
 	net.train_network()
 
 
@@ -200,7 +228,7 @@ def __test_neural_network__():
 	while not os.path.isfile(output_path):
 		open(output_path, 'wb')
 
-	net = MultiLayerPerceptron(weights_file=trained_weights, test_file=testing_set_path, output_file=output_path)
+	net = MLP(weights_file=trained_weights, test_file=testing_set_path, output_file=output_path)
 	net.test_network()
 
 
@@ -209,11 +237,11 @@ def dsig(x):
 
 
 def calculate_metrics(results):
-	accuracy_score = np.asarray((results[:, 0] + results[:, 3]) / np.sum(results, 1), dtype=np.float32)
+	accuracy = np.asarray((results[:, 0] + results[:, 3]) / np.sum(results, 1), dtype=np.float32)
 	precision = np.asarray((results[:, 0]) / np.sum(results[:, 0:2], 1), dtype=np.float32)
 	recall = np.asarray((results[:, 0]) / (results[:, 0] + results[:, 2]), dtype=np.float32)
 	f1 = np.asarray(2 * precision * recall / (precision + recall), dtype=np.float32)
-	return accuracy_score, precision, recall, f1
+	return accuracy, precision, recall, f1
 
 
 def generate_data_set():
@@ -254,13 +282,13 @@ def generate_data_set():
 try:
 	if __name__ == "__main__":
 		if len(sys.argv) < 2:
-			print("usage: python MLP.py [train|test]")
+			print("usage: python3 MLP.py [train|test]")
 		elif sys.argv[1] == 'test':
 			__test_neural_network__()
 		elif sys.argv[1] == 'train':
 			__train_neural_network__()
 		else:
-			print("usage: python MLP.py [train|test]")
+			print("usage: python3 MLP.py [train|test]")
 except KeyboardInterrupt:
 	sys.stdout.write("\nEncountered Ctrl + C. Exiting...\n")
 	sys.exit(0)
